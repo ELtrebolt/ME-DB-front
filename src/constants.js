a791dfb73e807e2f82b1f1c0d3c0f3c20d5eb5e9
Ethan Lee <ethanpl@uci.edu>
2023-08-17 16:46:56 -0700

    Server URL

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-// For Deployment:  https://me-db-server.onrender.com/
+// For Deployment:  https://me-db-server.onrender.com
 // For Local:       http://localhost:8082
 const json = {
-    'SERVER_URL': "https://me-db-server.onrender.com/",
+    'SERVER_URL': "https://me-db-server.onrender.com",
 };
 
 module.exports = json;
