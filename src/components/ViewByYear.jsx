3dcb8e94ca67d46f560440f7c196001e79e395ff
Ethan Lee <ethanpl@uci.edu>
2023-08-17 14:02:34 -0700

    Render URL

--- src/components/ViewByYear.jsx
+++ src/components/ViewByYear.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Function = ({possible_years, setFirstYear, setLastYear}) => {
+  const [firstYearValue, setFirstYearValue] = useState();
+  const [lastYearValue, setLastYearValue] = useState();
+
   const onChangeFirstYear = (e) => {
     setFirstYear(e.target.value );
+    setFirstYearValue(e.target.value);
   };
   
   const onChangeLastYear = (e) => {
     setLastYear(e.target.value );
+    setLastYearValue(e.target.value);
   };
 
   return (
     <><div className='col-md-2 mt-auto'>
       <div className='form-group'>
         <label htmlFor='firstYear'>First Year</label>
-        <select className='form-control' name='firstYear' onChange={onChangeFirstYear}>
+        <select className='form-control' name='firstYear' value={firstYearValue} onChange={onChangeFirstYear}>
           <option value={possible_years[0]}>Select a year</option>
           {possible_years.map((year) => (
             <option key={year} value={year}>
@@ -25,7 +30,7 @@ const Function = ({possible_years, setFirstYear, setLastYear}) => {
     </div><div className='col-md-2 mt-auto'>
         <div className='form-group'>
           <label htmlFor='lastYear'>Last Year</label>
-          <select className='form-control' name='lastYear' onChange={onChangeLastYear}>
+          <select className='form-control' name='lastYear' value={lastYearValue} onChange={onChangeLastYear}>
             <option value=''>Select a year</option>
             {possible_years.map((year) => (
               <option key={year} value={year}>
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
+// For Deployment:  https://me-db-server.onrender.com/
+// For Local:       http://localhost:8082
 const json = {
-    'SERVER_URL': "http://localhost:8082",
+    'SERVER_URL': "https://me-db-server.onrender.com/",
 };
 
 module.exports = json;
