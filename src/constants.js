cd1fe1f63fb25d1a905373ab6a4cc40dd6d30df1
Ethan Lee <ethanpl@uci.edu>
2023-08-20 23:20:37 -0700

    Vercel 1.1

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-// For Deployment:  https://me-db-server.onrender.com       https://me-db.cyclic.app.com  
+// For Deployment:  https://me-db-server.onrender.com       https://me-db.cyclic.cloud.com  
 // For Local:       http://localhost:8082
 const json = {
-    'SERVER_URL': "https://me-db.cyclic.app.com",
+    'SERVER_URL': "https://me-db.cyclic.cloud.com",
 };
 
 module.exports = json;
