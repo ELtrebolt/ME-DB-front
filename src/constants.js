8ffd62f279643ff898b3b38353b3327a59e5d8fd
Ethan Lee <ethanpl@uci.edu>
2023-08-31 12:46:18 -0700

    .Tech Server

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,8 @@
 // For Deployment:  https://me-db.cyclic.cloud
 // For Local:       http://localhost:8082
+// For Deployment:  https://www.me-db.tech
 const json = {
-    'SERVER_URL': "https://me-db.cyclic.cloud",
+    'SERVER_URL': "https://www.me-db.tech",
 };
 
 module.exports = json;
