0733ea41ce6c17b96d7573012c088c3349326188
Me <ethanpl@uci.edu>
2025-09-08 00:02:25 -0700

    me-db.app new domain

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 // For Local:       http://localhost:8082
-// For Deployment:  https://www.api.me-db.live
+// For Deployment:  https://www.api.me-db.app
 const json = {
-    'SERVER_URL': "http://localhost:8082",
+    'SERVER_URL': "https://www.api.me-db.app",
     'currentYear': new Date().getFullYear(),
     'examples': {
         'type' : 'e.g. Restaurants',
